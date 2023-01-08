f7ce37e40f2ad1abcb789092a331d3a84787f676
UltimateBoomer <caohc2017@gmail.com>
2023-01-08 15:38:10 -0500

    Base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        key={`square${i}`}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />)
+  }
+
+  render() {
+    const rows = []
+    for (let i = 0; i < this.props.dimensions[0]; i++) {
+      const row = []
+      for (let j = 0; j < this.props.dimensions[1]; j++) {
+        row.push(this.renderSquare(i * 3 + j));
+      }
+      rows.push(<div key={`row${i}`} className='board-row'>{row}</div>)
+    }
+    return <div>{rows}</div>
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(this.props.dimensions.reduce((a, b) => a * b)),
+        move: null,
+      }]
+    }
+  }
+
+  reset() {
+    this.setState((state) => ({
+      history: state.history.slice(0, 1)
+    }))
+  }
+
+  getCurrentPlayerName() {
+    return this.state.history.length % 2 ? 'X' : 'O'
+  }
+
+  getWins() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    const current = this.state.history.at(-1).squares
+    return lines
+      .filter(l => current[l[0]] && l.slice(1).every(i => current[i] === current[l[0]]))
+      .map(l => [current[l[0]], l])
+  }
+
+  handleClick(i) {
+    const current = this.state.history.at(-1).squares.slice()
+    if (this.getWins().length || current[i]) {
+      return
+    }
+
+    current[i] = this.getCurrentPlayerName()
+    this.setState((state) => ({ 
+      history: state.history.concat([{
+        squares: current,
+        move: i,
+      }])
+    }))
+  }
+
+  undo() {
+    if (this.state.history.length > 1) {
+      this.setState((state) => ({
+        history: state.history.slice(0, -1)
+      }))
+    }
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history.at(-1).squares
+    const wins = this.getWins()
+
+    let status
+    if (wins.length) {
+      status = `${wins[0][0]} wins!`
+    } else {
+      status = `${this.getCurrentPlayerName()}'s turn`
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            dimensions={this.props.dimensions}
+            squares={current}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <button onClick={() => this.undo()}>Undo</button>
+          <button onClick={() => this.reset()}>Reset</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Game dimensions={[3, 3]}/>)
